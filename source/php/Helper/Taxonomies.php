eedf86fbc9087439483f2a06a43d20ba5292d8af
NiclasNorin <niclas.norin@frontwalker.se>
2023-04-05 10:32:29 +0200

    class exists

--- source/php/Helper/Taxonomies.php
+++ source/php/Helper/Taxonomies.php
@@ -31,17 +31,19 @@ class Taxonomies
         foreach ($taxonomies as $taxonomy) {
             $terms = get_the_terms($postId, $taxonomy);
             if (!empty($terms)) {
-                foreach ($terms as $term) {
-                    if (empty($termIcon)) {
-                        $icon = \Municipio\Helper\Term::getTermIcon($term, $taxonomy);
-                        $color = \Municipio\Helper\Term::getTermColor($term, $taxonomy);
-                        if (!empty($icon) && !empty($icon['src']) && $icon['type' == 'icon']) {
-                            $termIcon['icon'] = $icon['src'];
-                            $termIcon['size'] = 'md';
-                            $termIcon['color'] = 'white';
-                        }
-                        if(!empty($color)) {
-                            $termIcon['backgroundColor'] = \Municipio\Helper\Term::getTermColor($term, $taxonomy);
+                if(class_exists('Municipio\Helper\Term')) {
+                    foreach ($terms as $term) {
+                        if (empty($termIcon)) {
+                            $icon = \Municipio\Helper\Term::getTermIcon($term, $taxonomy);
+                            $color = \Municipio\Helper\Term::getTermColor($term, $taxonomy);
+                            if (!empty($icon) && !empty($icon['src']) && $icon['type' == 'icon']) {
+                                $termIcon['icon'] = $icon['src'];
+                                $termIcon['size'] = 'md';
+                                $termIcon['color'] = 'white';
+                            }
+                            if(!empty($color)) {
+                                $termIcon['backgroundColor'] = \Municipio\Helper\Term::getTermColor($term, $taxonomy);
+                            }
                         }
                     }
                 }
