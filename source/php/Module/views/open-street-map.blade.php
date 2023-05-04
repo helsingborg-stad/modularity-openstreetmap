65aaba617f1876d4d57b29761edbf64f77210b5f
NiclasNorin <niclas.norin@frontwalker.se>
2023-05-04 16:49:57 +0200

    horizontal not vertical

--- source/php/Module/views/open-street-map.blade.php
+++ source/php/Module/views/open-street-map.blade.php
@@ -19,7 +19,7 @@
             @endtypography
         @endif
         @collection([
-            'classList' => ['o-grid', 'o-grid--vertical'],
+            'classList' => ['o-grid', 'o-grid--horizontal'],
             'attributeList' => [
                 'js-pagination-container' => '',
             ]
