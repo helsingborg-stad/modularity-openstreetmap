06171e32131984f467e690738537d88fa81045cd
NiclasNorin <niclasn@outlook.com>
2024-06-24 17:06:50 +0200

    feat: add modal

--- views/modal.blade.php
+++ views/modal.blade.php
@@ -1,79 +1,73 @@
 @modal([
     'closeButtonText' => 'Close',
-    'id' => 'modal-' . $place->id,
+    'id' => 'osm-' . $place->id,
     'isPanel' => true,
+    'padding' => 0,
 ])
-my content
-@endmodal
-
-{{-- @group([
-        'classList' => ['c-openstreetmap__post'],
-        'containerAware' => true,
-    ])
-    @icon([
-        'icon' => 'arrow_back',
-        'size' => 'md',
-        'color' => 'white',
-        'classList' => ['c-openstreetmap__post-icon'],
-    ])
-    @endicon
-    @if (!empty($place->images['thumbnail16:9']['src']))
+    @if(!empty($place->images['thumbnail16:9']['src']))
         @hero([
-            'image' => $place->images['thumbnail16:9']['src']
-        ])
+                'image' => $place->featuredImage['src'],
+            ])
         @endhero
     @endif
-    <div class="u-margin__x--2">
+        <div class="o-container">
         @paper([
             'attributeList' => [
-                'style' => !empty($place->images['thumbnail16:9']['src']) ? 'transform:translateY(calc(max(-50%, -50px)))' : 'margin-top: 32px'
+                'style' => !empty($place->featuredImage['src']) ? 'transform:translateY(calc(max(-50%, -50px)))' : 'margin-top: 32px'
             ],
-            'containerAware' => true,
-            'classList' => ['u-padding--6', 'o-container']
+            'classList' => ['u-padding--6']
         ])
-        @group([
-            'justifyContent' => 'space-between',
-        ])
-        @typography([
-            'element' => 'h1',
-            'variant' => 'h1'
+            @group([
+                'justifyContent'=> 'space-between',
             ])
-            {{ $place->postTitle }}
-        @endtypography
-        @if (!empty($place->callToActionItems['floating']))
-            @icon($place->callToActionItems['floating'])
-            @endicon
-        @endif
-        @endgroup
-            <div class="o-grid c-openstreetmap__post-container">
-                <div class="c-openstreetmap__post-content">
-                        @typography([
-                        ])
+                @typography([
+                    'element' => 'h1',
+                    'variant' => 'h1'
+                ])
+                    {!! $place->postTitleFiltered !!}
+                @endtypography
+                @if (!empty($place->callToActionItems['floating']))
+                    @icon($place->callToActionItems['floating'])
+                    @endicon
+                @endif
+            @endgroup
+            <div class="o-grid u-padding__top--4">
+                <div class="o-grid-12@sm o-grid-9@md o-grid-9@lg o-grid-9@xl">
+                    @typography([])
                         {!! $place->postContentFiltered !!}
-                        @endtypography
+                    @endtypography
                 </div>
-                @if($place->placeInfo)
-                <div class="c-openstreetmap__post-list">
-                    @listing([
-                        'list' => $place->placeInfo,
-                        'icon' => false,
-                        'classList' => ['unlist'],
-                        'padding' => 4,
+                <div class="o-grid-12@sm o-grid-3@md o-grid-3@lg o-grid-3@xl">
+
+                    @if (!empty($place->placeInfo))
+                        @listing([
+                            'list' => $place->placeInfo,
+                            'icon' => false,
+                            'classList' => [
+                                'unlist',
+                                'u-padding__top--2@xs',
+                                'u-padding__top--2@sm',
+                                'u-padding__bottom--3',
+                                'u-margin__top--2'
+                            ],
+                            'padding' => 4
                         ])
-                    @endlisting
+                        @endlisting
+                    @endif
+
+                    @if (!empty($place->bookingLink))
+                        @button([
+                            'text' => $lang->bookHere,
+                            'color' => 'primary',
+                            'style' => 'filled',
+                            'href' => $place->bookingLink,
+                            'classList' => ['u-width--100'],
+                        ])
+                        @endbutton
+                    @endif
+
                 </div>
-                @if (!empty($place->bookingLink))
-                    @button([
-                        'classList' => ['c-openstreetmap__post-button'],
-                        'text' => $lang->bookHere ?? 'Book here',
-                        'color' => 'primary',
-                        'style' => 'filled',
-                        'href' => $place->bookingLink,
-                    ])
-                    @endbutton
-                @endif
-                @endif
             </div>
         @endpaper
     </div>
-@endgroup --}}
+@endmodal
