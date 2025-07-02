6f350fb4f6f9ace36b9f5ce7bb8c5cbc1fb5db9b
NiclasNorin <niclasn@outlook.com>
2025-07-02 15:55:22 +0200

    fix: adding place post data

--- source/php/Api/PostTransformer/DefaultTransformer.php
+++ source/php/Api/PostTransformer/DefaultTransformer.php
@@ -15,6 +15,9 @@ class DefaultTransformer implements PostTransformerInterface {
         $post = (new ApplyOpenStreetMapDataToPostObject($post))->apply();
         $post->osmFilterValues = $this->addOsmFilteringCapabilities($post);
 
+        $fields = get_fields($post->getId() ?? 0) ?? [];
+        $post->postInfo = $this->createPlaceInfoList($fields);
+
         return $post;
     }
 
@@ -34,5 +37,43 @@ class DefaultTransformer implements PostTransformerInterface {
         }
 
         return $attributeString;
+    }
+
+     /**
+     * Create a list of place information based on specified fields.
+     *
+     * @param array $fields An array of fields containing information about the place.
+     *
+     * @return array The list of place information.
+     */
+    private function createPlaceInfoList($fields)
+    {
+        $list = [];
+        // Phone number
+        if (!empty($fields['phone'])) {
+            $list['phone'] = \Municipio\Helper\Listing::createListingItem(
+                $fields['phone'],
+                '',
+                ['src' => 'call']
+            );
+        }
+
+        // Website link (with fixed label)
+        if (!empty($fields['website'])) {
+            $list['website'] = \Municipio\Helper\Listing::createListingItem(
+                __('Visit website', 'municipio'),
+                $fields['website'],
+                ['src' => 'language'],
+            );
+        }
+
+        // Apply filters to listing items
+        $list = apply_filters(
+            'Municipio/Controller/SingularPlace/listing',
+            $list,
+            $fields
+        );
+
+        return $list;
     }
 }
