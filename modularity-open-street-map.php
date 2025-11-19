697b8a4cb9143f2f8dbe48fdf589a692919aae69
GitHub Action <>
2025-11-19 07:35:03 +0000

    build: update package file versions to 3.0.1

--- modularity-open-street-map.php
+++ modularity-open-street-map.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Modularity Open Street Map
  * Plugin URI:        https://github.com/NiclasNorin/modularity-open-street-map
  * Description:       A map based of OSM.
- * Version: 3.0.0
+ * Version: 3.0.1
  * Author:            Niclas Norin
  * Author URI:        https://github.com/NiclasNorin
  * License:           MIT
